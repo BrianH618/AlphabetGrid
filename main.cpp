0ff2d82180a405a41fec5eb3d2ab272b17d1f916
brianh618 <bherrera10@mail.csuchico.edu>
2018-05-14 12:21:51 -0700

    5/14/2018

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main()
 {
   int letters;
+  vector<vector<char> >alphabet;
+
 
   do
   {
@@ -17,6 +19,41 @@ int main()
       cout<<"\nPlease enter a number between 1-26.\n";
     }
   }while( letters < 1 || letters > 26 );
+    int rows;
+    if (26 % letters ==0)
+    {
+        rows=26%letters;
+    }
+    else
+    {
+        rows = 26/letters +1;
+    }
+
+    alpabet. resize(letters);
+    for(int row =0; row <alpgabet.size(); row++)
+    {
+      alphabet[row].resize(letters);
+    }
+
+    char currentLetter='A';
+    for(int row =0; row<alpgabet.size(); row++)
+    {
+        for(int col=0; col< alpgabet.size(); row++)
+        {
+            alpgabet[row][col] = currentLeter;
+            currentLetter++; // increment to next letter in the ascii talble
+            if(currentLetter>'Z')
+            {
+                currentLetter = 'Z';
+            }
+        }
+    }
+
+    //display matrix
+    for(int row=0; row<alphabet.size(); row++)
+    {
+        for (int col=0; col< alphabet[row].size(); col++)
+    }
 
   return 0;
 }
